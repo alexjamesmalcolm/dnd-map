15a975b78b0dff5df8ce23e836e5fc0367c3f79f
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-09-23 18:23:05 -0400

    Moved contents of App.tsx to Example.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,7 @@
-import { useCallback, useState } from "react";
-import usePeerState from "./hooks/usePeerState";
-import useReceivePeerState from "./hooks/useReceivePeerState";
-
-interface State {
-  value: string;
-}
-const initialState: State = {
-  value: "",
-};
+import Example from "./Example";
 
 const App = () => {
-  const [brokerIdOfSomeoneElse, setBrokerIdOfSomeoneElse] = useState<string>();
-  const { state, setState, brokerId, error, connections } =
-    usePeerState(initialState);
-  console.log(connections);
-  const {
-    state: stateOfSomeoneElse,
-    isConnected,
-    error: errorOfSomeoneElse,
-  } = useReceivePeerState<State>(brokerIdOfSomeoneElse);
-  const handleBrokerIdChange = useCallback<
-    React.ChangeEventHandler<HTMLInputElement>
-  >((event) => {
-    setBrokerIdOfSomeoneElse(event.target.value);
-  }, []);
-  const handleStateChange = useCallback<
-    React.ChangeEventHandler<HTMLInputElement>
-  >(
-    (event) => {
-      const newState: State = { value: event.target.value };
-      setState(newState);
-    },
-    [setState]
-  );
-  return (
-    <div>
-      <pre>{JSON.stringify({ state, brokerId, error }, null, 2)}</pre>
-      <pre>
-        {JSON.stringify(
-          { stateOfSomeoneElse, isConnected, errorOfSomeoneElse },
-          null,
-          2
-        )}
-      </pre>
-      <label>
-        Broker ID
-        <input onChange={handleBrokerIdChange} />
-      </label>
-      <label>
-        State
-        <input onChange={handleStateChange} />
-      </label>
-    </div>
-  );
+  return <Example />;
 };
 
 export default App;
