9d890e22e07e7b859f6d0dff8a33527b313a0782
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-09-23 18:28:58 -0400

    Wrapped Example in Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Example from "./Example";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const App = () => {
-  return <Example />;
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/">
+        <Example />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
